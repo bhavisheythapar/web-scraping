84596af57cc0a68dc8ae6d0154da6d5470b8e3ac
Thapar, Bhavishey <bhavishey.thapar@mda.space>
2022-07-06 11:30:41 -0400

    Updated Code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 page = requests.get("https://www.udacity.com/courses/all?price=Free",verify=False)
 soup = BeautifulSoup(page.content, 'html.parser')
-# a=soup.find(id="#content")
-# b=a.find_all(class_="catalog-v2_page__187xz")
-# c=b.find_all(class_="container_contain__2hCQ1")
-# d=c[0].find_all(class_="catalog-v2_results__1FjDi")
-for ultag in soup.find_all('ul', {'class': 'catalog-v2_results__1FjDi'}):
-    for litag in ultag.find_all('li'):
-        print (litag.text)
+ul=soup.find('ul', {'id': 'catalog-v2_results__1FjDi'})
+print(ul)
+# children = ul.findChildren("a" , recursive=False)
+# for child in children:
+#     print(child)
+# for item in soup.findAll('ul',{'class':'catalog-footer_linkGroupItems__1wmH6'}):
+#     print(item)
 
+# for item in soup.findAll('div',{'class':'container_contain__2hCQ1'}):
+#     sub_items = item.findAll('li',{'class':'href text'})
+#     for sub_item in sub_items:
+#         print(sub_item)
