113a2e20763ad73e534eb3a5087edb7f90cf95cb
Bhavishey Thapar <bhavisheythapar@gmail.com>
2022-07-08 00:20:17 -0400

    Updated file

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Web scraping
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
  
 driver.get('https://www.udacity.com/courses/all?price=Free')
@@ -30,6 +32,7 @@ for course in soup.select('.catalog-v2_results__1FjDi > li'):
     summary_list.append(summary)
     level_list.append(level)
 
+# Create dataframe and transfer data to it.
 df=pd.DataFrame(
     {'title': title_list,
      'duration': duration_list,
@@ -37,6 +40,7 @@ df=pd.DataFrame(
      'level': level_list
     })
 
+# Create a database and transfer data to it.
 conn = sqlite3.connect('Udacity')
 c = conn.cursor()
 conn.commit()
